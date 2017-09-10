37b5b521b8d106e377ead84f9cce26d5b79d0446
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-10 20:07:06 -0300

    Section 11, class 152

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -6,13 +6,39 @@ const cara = require('../imgs/moeda_cara.png');
 const coroa = require('../imgs/moeda_coroa.png');
 
 export default class Resultado extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { resultado: '' };
+    }
+
+    componentWillMount() {
+        const numAleatorio = Math.floor(Math.random() * 2);
+        let caraOuCoroa = '';
+
+        if (numAleatorio === 0) {
+            caraOuCoroa = 'cara';
+        } else {
+            caraOuCoroa = 'coroa';
+        }
+
+        this.setState({ resultado: caraOuCoroa });
+    }
+
     render() {
-        return(
-            <View style={styles.resultado}>
-                <Image source={cara} />
-                <Image source={coroa} />
-            </View>
-        );
+        if (this.state.resultado === 'cara') {
+            return(
+                <View style={styles.resultado}>
+                    <Image source={cara} />
+                </View>
+            );
+        } else {
+            return(
+                <View style={styles.resultado}>
+                    <Image source={coroa} />
+                </View>
+            );
+        }
     }
 }
 
