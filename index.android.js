941a94614316bda1f65523409803eb2a2581435f
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-10 19:13:30 -0300

    Section 11, class 148

--- index.android.js
+++ index.android.js
@@ -5,52 +5,16 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Image
-} from 'react-native';
+import { AppRegistry } from 'react-native';
 
-const logo = require('./src/imgs/logo.png');
-const btnJogar = require('./src/imgs/botao_jogar.png');
-const btnSobreJogo = require('./src/imgs/sobre_jogo.png');
-const btnOutrosJogos = require('./src/imgs/outros_jogos.png');
+import Principal from './src/components/Principal';
 
 export default class game_cara_ou_coroa extends Component {
   render() {
     return (
-        <View style={styles.cenaPrincipal}>
-            <View style={styles.apresentacaoJogo}>
-                <Image source={logo} />
-                <Image source={btnJogar} />
-            </View>
-
-            <View style={styles.rodape}>
-                <Image source={btnSobreJogo} />
-                <Image source={btnOutrosJogos} />
-            </View>
-        </View>
+        <Principal />
     );
   }
 }
-
-const styles = StyleSheet.create({
-    cenaPrincipal: {
-        flex: 1,
-        backgroundColor: '#61BD8C'
-    },
-    apresentacaoJogo: {
-        flex: 10,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    rodape: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    }
-});
 
 AppRegistry.registerComponent('game_cara_ou_coroa', () => game_cara_ou_coroa);
